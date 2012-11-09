22a996b573d7a3a3004d9a397c6dea21d751e59f
tarik (Tarik Gouhier) <tarik.gouhier@gmail.com>
2012-11-09 22:40:17 +0000

    SIgnificance testing for peaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synchrony@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/peaks.signif.R
@@ -0,0 +1,14 @@
+peaks.signif <-
+function  (t1, t2, nrands=999) {
+  observed.peaks=peaks (t1, t2)
+  randomized.peaks=numeric(length=nrands+1)
+  for (n in 1:nrands) {
+    t1.tmp=cbind(t1[,1], sample(t1[,2]))
+    t2.tmp=cbind(t2[,1], sample(t2[,2]))
+    randomized.peaks[n]=peaks(t1.tmp, t2.tmp)$peaks
+  }
+  randomized.peaks[n+1]=observed.peaks$peaks
+  pval=sum(randomized.peaks >= observed.peaks$peaks)/(nrands+1)
+  return (list(pval=pval, rands=randomized.peaks, peaks=observed.peaks$peaks, 
+               locations=observed.peaks$locations))
+}
