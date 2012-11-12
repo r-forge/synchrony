297438de63c13e5067e369f88851c05de38c4f3d
tarik (Tarik Gouhier) <tarik.gouhier@gmail.com>
2012-11-12 08:28:42 +0000

    Add new function community.matrix to generate a matrix with a specified correlation, mean, and sd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synchrony@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/correlated.matrix.R
@@ -0,0 +1,11 @@
+correlated.matrix <- function (rho = 0, sigma=1, mu=0, ntimes = 200, nspecies = 10) {
+  U=matrix(rho, nrow=nspecies, ncol=nspecies)
+  diag(U)=1
+  # Cholesky decomposition
+  A=chol(U)
+  community=matrix(rnorm(ntimes*nspecies, sd=1, mean=0), nrow=ntimes, ncol=nspecies) %*%A
+  community=scale(community)*sigma+mu
+
+  results=list(rho=rho, sigma=sigma, mu=mu, community=community)
+  return (results)  
+}
