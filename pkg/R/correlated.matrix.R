bc297b0d8d475f5990da09f6fbcdecc496e1ba18
tarik (Tarik Gouhier) <tarik.gouhier@gmail.com>
2012-11-13 04:19:26 +0000

    Remove attributes frome scaled correlated.matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synchrony@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/correlated.matrix.R
+++ pkg/R/correlated.matrix.R
@@ -4,8 +4,10 @@ correlated.matrix <- function (rho = 0, sigma=1, mu=0, ntimes = 200, nspecies = 
   # Cholesky decomposition
   A=chol(U)
   community=matrix(rnorm(ntimes*nspecies, sd=1, mean=0), nrow=ntimes, ncol=nspecies) %*%A
-  community=scale(community)*sigma+mu
-
+  community=scale(community, center=TRUE, scale=TRUE)*sigma+mu
+  attr(community, "scaled:center")=NULL
+  attr(community, "scaled:scale")=NULL
+  
   results=list(rho=rho, sigma=sigma, mu=mu, community=community)
   return (results)  
 }
