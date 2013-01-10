b0c0788b02c8e5a1140dab18d711af64ff752420
tarik (Tarik Gouhier) <tarik.gouhier@gmail.com>
2013-01-10 17:11:07 +0000

    Switch from .onLoad to .onAttach
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synchrony@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     paste("synchrony", 
           utils::packageDescription("synchrony",
