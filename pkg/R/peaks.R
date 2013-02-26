aef6885b9ab793f5baba6a630913cbcd39a75758
tarik (Tarik Gouhier) <tarik.gouhier@gmail.com>
2013-02-26 06:52:50 +0000

    Fix minor bug in peaks function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synchrony@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/peaks.R
+++ pkg/R/peaks.R
@@ -26,14 +26,14 @@ peaks.aux <- function (t1, t2) {
   f1=find.minmax(t1)
   f2=find.minmax(t2)
   
-  common.mins=f1$mins$location %in% f2$mins$location
-  common.maxs=f1$maxs$location %in% f2$maxs$location
+  common.mins=f1$mins$index %in% f2$mins$index
+  common.maxs=f1$maxs$index %in% f2$maxs$index
   peaks=(sum(common.mins)+sum(common.maxs))/sum(max(NROW(f1$mins), 
                                                     NROW(f2$mins)) +
                                                   max(NROW(f1$maxs), 
                                                       NROW(f2$maxs)))
   
-  locations=sort(c(f1$mins$location[common.mins], 
-                   f1$maxs$location[common.maxs]))
-  return (list(peaks=peaks, locations=locations))
+  index=sort(c(f1$mins$index[common.mins], 
+                   f1$maxs$index[common.maxs]))
+  return (list(peaks=peaks, index=index))
 }
